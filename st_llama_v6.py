615d1b9329dd92c830c3b33bade09270d964b34e
florian <florian.mehnert@gmail.com>
2024-06-30 12:07:24 +0200

    :wizard: Ah, brave Tarnished. The code now shines with brilliance!

--- st_llama_v6.py
+++ st_llama_v6.py
@@ -53,12 +53,14 @@ end_token_role = "<|end_header_id|>"
 end_token_input = "<|eot_id|>"
 
 with st.sidebar:
-    st.logo('https://i.kym-cdn.com/entries/icons/original/000/000/043/dg1.jpg')
+    st.logo('logo.svg')
     with st.expander("**Predefined questions**"):
-        s1 = st.text_input("system 1", key="s1", value="The following text is missing one or multiple words. Your task is to listen to the following tasks. ")
-        q1 = st.text_input("question 1", key="q1", value="Correct this text. Only respond with the corrected text. Do not add any summarization.")
-        q2 = st.text_input("question 2", key="q2", value="Try to improve on your text!")
-        q3 = st.text_input("question 3", key="q3", value="Improve your text further!")
+        s1 = st.text_area("system 1", key="s1", value="The following text is missing one or multiple words. Your task is to listen to the following tasks. ")
+        q1 = st.text_input("question 1", key="q0", value="Correct this text. Only respond with the corrected text. Do not add any summarization.")
+        q2 = st.text_input("question 2", key="q1", value="Try to improve on your text!")
+        q3 = st.text_input("question 3", key="q2", value="Improve your text further!")
+    # predefine user input OwO
+    st.session_state.user_msgs = [key for key in st.session_state.keys() if key.startswith("q")]
     with st.expander("**LLM Parameters**"):
         st.session_state.temperature = st.slider("**Temperature:** by default 0.97 but adjust to your needs:", min_value=0.0, value=0.97, max_value=10.0)
         st.session_state.num_predict = st.slider("**Max tokens**: Maximum amount of tokens that are output:", min_value=128, value=128, max_value=2048)
@@ -85,7 +87,7 @@ def assemble_pre_prompt(idx: int) -> str:
     # get all messages in order for current conversation thread
     prompt: str = system_prompt()
     for i in range(st.session_state.count + 1):
-        prompt += st.session_state.user_msgs[i] if st.session_state.user_msgs[idx] else ""  # is changing
+        prompt += st.session_state.user_msgs[i]
         prompt += end_token_input
         prompt += str(Roles.assistant.value)
 
@@ -120,56 +122,61 @@ def stream_response(idx: int):
             st.session_state.has_finished = True
 
 
+def provide_current_user_input(idx: int):
+    """
+    general inputs consist of system input (stays fixed) and user inputs (can be overwritten later)
+    user input are defined as chain of thoughts in the predefined questions section
+    based on the current count select the correct question e.g. count is 0 select st.session_state.q1
+    """
+    if f"q{idx}" in st.session_state:
+        return st.session_state[f'q{idx}']  # :O
+
+
 def main():
     # title
     st.logo("https://ollama.com/public/ollama.png")
     st.title(f"Llama {"".join([":llama:" for _ in range(3)])} playground")
 
-    html = ds.scrape_webpage("https://www.gutenberg.org/files/701/701-h/701-h.htm#chap01") # collect_website
-    content = ds.extract_content(html)  # initial text
+    html = ds.scrape_webpage("https://www.gutenberg.org/files/701/701-h/701-h.htm#chap01")  # collect_website
+    content: list[str] = ds.extract_content(html)  # initial text
     content = ds.process_text(content)  # split into paragraphs
-    testing_message = [ds.create_gaps(s, st.session_state.mask_rate) for s in content]  # remove parts based on seed etc.
+    paragraphs: list[str] = [ds.create_gaps(s, st.session_state.mask_rate) for s in content]  # remove parts based on seed etc.
 
     # buttons
-    col1, col2 = st.columns(2)
-    with col1:
-        start_computation = st.button("Start computation")
-    with col2:
-        if st.button("Reset"):
-            clear_cache(True)
+    start_computation = st.button("Start computation")
+    pre_stop = 0
+    for paragraph in paragraphs:
+        if pre_stop > 2:
+            break
+        pre_stop += 1
+        st.session_state.system = st.session_state.s1 + paragraph  # add user prompt and system message
+        for question_number in range(len(st.session_state.user_msgs)):
+            if start_computation:
+                if st.session_state.has_finished:  # completed once
+                    st.session_state.count += 1  # using this to access current position in msgs arrays
 
-    if start_computation:
-        if st.session_state.has_finished:  # completed once
-            st.session_state.count += 1  # using this to access current position in msgs arrays
+                    try:
+                        st.session_state.user_msgs[st.session_state.count]
+                    except IndexError:
+                        st.session_state.user_msgs.append("")
 
-            try:
-                st.session_state.user_msgs[st.session_state.count]
-            except IndexError:
-                st.session_state.user_msgs.append("")
+                    for assistant in st.session_state.assistant_msgs:
+                        try:
+                            assistant[st.session_state.count]
+                        except IndexError:
+                            assistant.append("")
 
-            for assistant in st.session_state.assistant_msgs:
-                try:
-                    assistant[st.session_state.count]
-                except IndexError:
-                    assistant.append("")
+                    st.session_state.has_finished = False
 
-            st.session_state.has_finished = False
-            st.session_state.user_msgs[st.session_state.count] = testing_message[st.session_state.count]  # static
+                    # appending response from previous iteration
+                    st.session_state.assistant_msgs[0][st.session_state.count - 1] = st.session_state.response[0]
 
-            # appending response from previous iteration
-            st.session_state.assistant_msgs[0][st.session_state.count - 1] = st.session_state.response[0]
+                    st.session_state.response = ["" for _ in range(st.session_state.amount_of_responses)]
 
-            st.session_state.response = ["" for _ in range(st.session_state.amount_of_responses)]
-
-        # with nth count go to nth gapped_passage entry
-        st.session_state.prompt = testing_message[st.session_state.count]
-
-        # add user msg
-        st.session_state.user_msgs[st.session_state.count] = st.session_state.prompt
-
-        # generate text for nth passage entry
-        if not st.session_state.has_finished:
-            st.write(stream_response(0))
+                # generate text for nth passage entry
+                if not st.session_state.has_finished:
+                    st.write(stream_response(0))
+    st.write(st.session_state.q1)
 
 
 if __name__ == "__main__":
